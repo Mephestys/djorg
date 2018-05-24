3903486cb0d7038674570db91e2cc9d6303c96c1
Mephestys <mephestys@gmail.com>
2018-05-24 14:22:08 -0600

    Added personal bookmarks for individual users, new bookmark form - Personal bookmark database accessible to admin - Added simple root page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 
 def index(request):
-  context = {
-    'bookmarks': Bookmark.objects.all(),
-  }
+
+  if request.method == 'POST':
+    form = BookmarkForm(request.POST)
+    if form.is_valid():
+      form.save()
+    else:
+      # TODO error
+      pass
+
+  context = {}
+
+  pbid = PersonalBookmark.objects.values_list('id')
+  
+  context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
+
+  if request.user.is_anonymous:
+    context['personal_bookmarks'] = PersonalBookmark.objects.none()
+  else:
+    context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+  context['form'] = BookmarkForm
 
   return render(request, 'bookmarks/index.html', context)
