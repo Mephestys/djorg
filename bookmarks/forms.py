bce396ec4eca70024fb2ada61630dc379345488c
Mephestys <mephestys@gmail.com>
2018-05-25 09:16:50 -0600

    Slightly cleaner bookmarks index page, starting on personal bookmarks - Has a probably non-functional class for personal bookmarks, that's all

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 class BookmarkForm(forms.ModelForm):
-
   class Meta:
     model = Bookmark
     fields = ('url', 'name', 'notes')
+
+class PersonalBookmarkForm(BookmarkForm):
+  class Meta:
+    model = PersonalBookmark
+    fields = BookmarkForm.Meta.fields
