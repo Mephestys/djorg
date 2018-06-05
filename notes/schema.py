e67fb7889ddad810662d773135ce59aea545be88
Mephestys <mephestys@gmail.com>
2018-06-05 13:35:45 -0600

    Added GraphQL note retrieval and creation from lecture

--- notes/schema.py
+++ notes/schema.py
@@ -9,9 +9,10 @@ class Note(DjangoObjectType):
     interfaces = (graphene.relay.Node, )
 
 class Query(graphene.ObjectType):
-  notes = graphene.List(Note)
+  note = graphene.List(Note, id=graphene.String(), title=graphene.String())
+  all_notes = graphene.List(Note) # The framework will convert this to camelCase :/
 
-  def resolve_notes(self, info):
+  def resolve_all_notes(self, info):
     """Decide which notes to return."""
 
     user = info.context.user
@@ -23,4 +24,38 @@ class Query(graphene.ObjectType):
     else:
       return NoteModel.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+  def resolve_note(self, info, **kwargs):
+    title = kwargs.get('title')
+    id = kwargs.get('id')
+
+    if title is not None:
+      return NoteModel.objects.filter(title=title)
+    else:
+      return None
+
+class CreateNote(graphene.Mutation):
+  class Arguments:
+    # Input fields
+    title = graphene.String()
+    content = graphene.String()
+  
+  # Output fields
+  ok = graphene.Boolean()
+  note = graphene.Field(Note)
+
+  def mutate(self, info, title, content):
+    user = info.context.user
+
+    if user.is_anonymous:
+      is_ok = False
+      return CreateNote(ok=is_ok)
+    else:
+      new_note = NoteModel(title=title, content=content, user=user)
+      new_note.save()
+      is_ok = True
+      return CreateNote(note=new_note, ok=is_ok)
+
+class Mutation(graphene.ObjectType):
+  create_note = CreateNote.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
